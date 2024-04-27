4b0c207936a651b3958ae5793e199ddc9c29b55b
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-27 23:53:46 +0300

    work

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  const handleNameChange = (e) => {
+    setItemName(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setItemCategory(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+    onItemFormSubmit(newItem);
+    setItemName("");
+    setItemCategory("Produce");
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={itemName}
+          onChange={handleNameChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={itemCategory}
+          onChange={handleCategoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
